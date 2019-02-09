b8d6374fe7243d16ea5724e3b70adb55c513f4da
Sarah Lopez <31353768+sarahlopezsl@users.noreply.github.com>
2019-02-08 21:20:06 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/lab1_affectiva_android/MainActivity.java
+++ app/src/main/java/com/example/lab1_affectiva_android/MainActivity.java
@@ -92,7 +92,6 @@ public class MainActivity extends AppCompatActivity implements CameraDetector.Ca
 
 
         //5
-        float first_time = 0;
         float avg_t = 0;
 
         //************ TIME **************
